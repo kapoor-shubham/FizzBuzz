8eb501d6ddf5143c3e1b97c0362d5a60918e1e10
Shubham Kapoor <Shubham.Kapoor@arris.com>
2020-01-02 11:56:58 +0530

    Fizz Buzz Optimisation

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
 
-var str = "Hello, playground"
+let number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 30]
+
+// For Fizz and Buzz Seprately
+for num in number {
+    if num % 3 == 0 {
+        print("\(num) fizz")
+    } else if num % 5 == 0 {
+        print("\(num) buzz")
+    } else {
+        print(num)
+    }
+}
+
+// If a number is Fizz and Buzz Both than show FizzBuzz
+for num in number {
+//    if num % 3 == 0 && num % 5 == 0 {
+    if num % 15 == 0 {      // Instead of Running code Two Times if it is divisible by 3 and again buy 5 Optimise it if it is divisible by 15 so that if will only run once, instead of twice.
+        print("\(num) fizz buzz")
+    } else if num % 3 == 0 {
+        print("\(num) fizz")
+    } else if num % 5 == 0 {
+        print("\(num) buzz")
+    } else {
+        print(num)
+    }
+}
